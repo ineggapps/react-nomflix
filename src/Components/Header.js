825f3a9e0234b3fffa4ac5d487df13073981d139
inegg <inegg.apps@gmail.com>
2019-05-14 18:03:50 +0900

    Header Component has been initialized by styled-components.
    Header Component has been initialized by styled-components.
    
    * Link can be used only in <Router> tag.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Header = styled.header``;
+
+const List = styled.ul`
+  display: flex;
+  &:hover {
+    background-color: blue;
+  }
+`;
+
+const Item = styled.li``;
+
+const SLink = styled(Link)``;
 
 export default () => (
-  <header>
-    <ul>
-      <li>
-        <a href="/">Movies</a>
-      </li>
-      <li>
-        <a href="/tv">TV shows</a>
-      </li>
-      <li>
-        <a href="/search">Search</a>
-      </li>
-    </ul>
-  </header>
+  <Header>
+    <List>
+      <Item>
+        <SLink to="/">Movies</SLink>
+      </Item>
+      <Item>
+        <SLink to="/tv">TV shows</SLink>
+      </Item>
+      <Item>
+        <SLink to="/search">Searchd</SLink>
+      </Item>
+    </List>
+  </Header>
 );
